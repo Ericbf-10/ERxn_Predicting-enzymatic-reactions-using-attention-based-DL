badd89d4c2efe43259018cf020753cac760e6de3
jfunk-biodesign <funk.jonathan21@gmail.com>
2022-06-17 20:41:47 +0200

    bringing enzymes in tabular form

---
+++ scripts/01_clean_data.py
@@ -0,0 +1,42 @@
+import os
+import pandas as pd
+
+script_path = os.path.dirname(__file__)
+data_dir = os.path.join(script_path, '../data')
+raw_data_dir = os.path.join(data_dir, 'raw')
+raw_data_file = os.path.join(raw_data_dir, 'enzyme.dat')
+dest = os.path.join(data_dir, 'processed')
+
+ID_and_uniprot = {}
+with open(raw_data_file, 'r') as f:
+    for line in f:
+        if line.startswith('ID'):
+            ID = line[5:].replace('\n', '')
+            ID_and_uniprot[ID] = []
+
+        if line.startswith('DR'):
+            if len(line[5:11]) > 1:
+                ID_and_uniprot[ID].append(line[5:11])
+            if len(line[27:33]) > 1:
+                ID_and_uniprot[ID].append(line[5:11])
+            if len(line[49:55]) > 1:
+                ID_and_uniprot[ID].append(line[5:11])
+
+IDs = []
+ECs = []
+
+for EC in ID_and_uniprot:
+    proteins = ID_and_uniprot[EC]
+    if len(proteins) < 1:
+        pass
+    else:
+        for protein in proteins:
+            ECs.append(EC)
+            IDs.append(protein)
+
+data = pd.DataFrame({
+    'protein': IDs,
+    'EC': ECs
+})
+
+data.to_csv(f'{dest}/01_uniprot_and_EC.csv', index=None, sep=',')
