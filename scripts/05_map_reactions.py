a7f78ad0f128b56f9831bcf02dff6769523387f4
jonfunk21 <funk.jonathan21@gmail.com>
2022-08-03 15:02:57 +0200

    reactions as SMILES

--- scripts/05_map_reactions.py
+++ scripts/05_map_reactions.py
@@ -1,7 +1,5 @@
 import os
-import pubchempy as pc
 import pandas as pd
-from tqdm import tqdm
 import pickle
 
 script_path = os.path.dirname(__file__)
@@ -18,6 +16,7 @@ def split_reaction(reaction):
     educts = educts.split(' + ')
     products = products.split(' + ')
     compounds = educts + products
+    compounds_with_stoichiometry = compounds
 
     # handeling stoichiometry
     for i in range(20):
@@ -34,7 +33,7 @@ def split_reaction(reaction):
         if compound.startswith('n '):
             compounds[i] = compound[len('n '):]
 
-    return compounds
+    return compounds, compounds_with_stoichiometry
 
 with open(synonyms_path, 'rb') as f:
     synonyms = pickle.load(f)
@@ -43,63 +42,69 @@ reactions = pd.read_csv(reactions_data)
 
 smile_data = pd.read_csv(compounds_smile_data)
 compounds_with_smiles = smile_data['name'].to_list()
-
-r_list = []
-for i, reaction in enumerate(reactions['reaction'].to_list()):
-    for synonym in synonyms.keys():
-        reaction = reaction.replace(synonym, synonyms[synonym])
-    r_list.append(reaction)
-
 reactions = reactions.reset_index()
 partially_covered_reactions = 0
 fully_covered_reactions = 0
 dropped_reactions = 0
-remove_unknown = True
+
 reactions_kept = []
 fully_covered = []
 for i, reaction in reactions.iterrows():
     reaction = reaction['reaction']
     try:
-        compounds = split_reaction(reaction)
+        compounds, compounds_with_stoichiometry = split_reaction(reaction)
         n_compounds = len(compounds)
         count = 0
-        for compound in compounds:
-            if compound in synonyms.keys():
-                reaction = reaction.replace(compound, synonyms[compound])
 
-        for compound in compounds:
+        # remove everything that has no smile
+        for j, compound in enumerate(sorted(compounds, key=len, reverse=True)):
+            if compound in synonyms.keys():
+                compound = synonyms[compound]
+            if compound not in compounds_with_smiles:
+                reaction = reaction.replace(sorted(compounds, key=len, reverse=True)[j], '')
+
+        for j, compound in enumerate(compounds):
+            if compound in synonyms.keys():
+                synonym = synonyms[compound]
+                synonym_with_stoichiometry = compounds_with_stoichiometry[j].replace(compound, synonym)
+                reaction = reaction.replace(compounds_with_stoichiometry[j], synonym_with_stoichiometry)
+                compounds[j] = synonym
+                compounds_with_stoichiometry[j] = synonym_with_stoichiometry
+
+        # case first reactant is removed
+        if reaction.startswith(' + '):
+            reaction = reaction[3:]
+
+        # first product is removed
+        if '  + ' in reaction:
+            reaction = reaction.replace('  + ', ' ')
+
+        # last educt is removed
+        if ' +  ' in reaction:
+            reaction = reaction.replace(' +  ', ' ')
+
+        # intermediate product or educt is removed
+        if '+  +' in reaction:
+            reaction = reaction.replace('+  +', '+')
+
+        for j, compound in enumerate(compounds):
             if compound in compounds_with_smiles:
                 reaction = reaction.replace(compound, smile_data.loc[smile_data['name'] == compound, 'smile'].item())
                 count += 1
-            else:
-                if remove_unknown:
-                    reaction = reaction.replace(compound, '')
 
-        if remove_unknown == True:
-            # case first reactant is removed
-            if reaction.startswith(' + '):
-                reaction = reaction[3:]
-
-            # first product is removed
-            if '  + ' in reaction:
-                reaction = reaction.replace('  + ', ' ')
-
-            # last educt is removed
-            if ' +  ' in reaction:
-                reaction = reaction.replace(' +  ', ' ')
-
-            # intermediate product or educt is removed
-            if '+  +' in reaction:
-                reaction = reaction.replace('+  +', '+')
-
-            # last product removed
-            if reaction.endswith(' + '):
-                reaction = reaction[:-3]
+        # last product removed
+        if reaction.endswith(' + '):
+            reaction = reaction[:-3]
 
         if count == n_compounds:
             fully_covered_reactions += 1
             reactions_kept.append(reaction)
             fully_covered.append(1)
+
+        elif count == 0:
+            dropped_reactions += 1
+            reactions.drop(index=i, axis=0, inplace=True)
+
         else:
             partially_covered_reactions += 1
             reactions_kept.append(reaction)
