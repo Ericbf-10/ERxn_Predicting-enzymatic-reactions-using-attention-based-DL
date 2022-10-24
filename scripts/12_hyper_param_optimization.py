dd4cfdae74adfbdf421f240b243992159a3b7c08
Ericbf10 <eric.bautista01@estudiant.upf.edu>
2022-10-24 18:01:34 +0200

    Fix directory path and finish script to benchmark hyper-parameters

--- scripts/12_hyper_param_optimization.py
+++ scripts/12_hyper_param_optimization.py
@@ -93,6 +93,6 @@ for i in range(len(hyper_param_list)):
             outfile_list.pop()
             out_file = out_file + ".txt"
             if out_file not in outfile_list:
-                time.sleep(5)
+                time.sleep(1800)
             else:
                 flag = False
