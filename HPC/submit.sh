09d112e9b6554079f3c2c9a159a7eec6130fbadd
jonfunk21 <funk.jonathan21@gmail.com>
2022-11-12 14:25:41 +0100

    run mol_trans

--- HPC/submit.sh
+++ HPC/submit.sh
@@ -33,8 +33,8 @@ module load pandas/1.3.1-python-3.7.11
 module load numpy/1.21.1-python-3.7.11-openblas-0.3.17
 
 #pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
-pip3 install torch
-pip3 install torchtext
+#pip3 install torchtext
+pip3 install torch==1.12.0 torchtext==0.13.0
 pip3 install --upgrade pip
 pip3 install  networkx
 pip3 install --user matplotlib
