055abc061bc43b7ccfed49f8b107ea0626a02547
jonfunk21 <funk.jonathan21@gmail.com>
2022-11-18 14:58:18 +0100

    git push

--- scripts/13_mol_transformer.py
+++ scripts/13_mol_transformer.py
@@ -76,7 +76,7 @@ valid_dataset = RxnDataset(src_valid_data_path,
                            vocab
                           )
 
-BATCH_SIZE = 2048
+BATCH_SIZE = 1024
 
 train_loader = torch.utils.data.DataLoader(
     train_dataset,
