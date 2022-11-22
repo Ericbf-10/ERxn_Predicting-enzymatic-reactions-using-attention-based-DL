62ee916b4ec938a5313262e92f0d6ddbfdc73a1b
jonfunk21 <funk.jonathan21@gmail.com>
2022-11-22 15:39:42 +0100

    train protein encoder with SGD

--- HPC/submit.sh
+++ HPC/submit.sh
@@ -48,4 +48,4 @@ pip install -U scikit-learn
 #python3 /zhome/4c/8/164840/projects/ERxn/scripts/10_protein_autoencoder.py
 
 #python3 /zhome/4c/8/164840/projects/ERxn/scripts/13_mol_transformer.py
-python3 /zhome/4c/8/164840/projects/ERxn/scripts/11_protein_encoder.py
+python3 /zhome/4c/8/164840/projects/ERxn/scripts/11_protein_encoder.py -optim sgd
